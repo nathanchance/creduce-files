05085a2632d31a4f44ab4434dbbe1f21e9d19ef4
Nathan Chancellor <natechancellor@gmail.com>
2020-03-23 23:30:01 -0700

    creduce-files: fedora-ppc64le-amdgpu: Results of reduction
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- fedora-ppc64le-amdgpu/test.sh
+++ fedora-ppc64le-amdgpu/test.sh
@@ -2,4 +2,4 @@
 
 /home/nathan/toolchains/gcc/9.3.0/bin/powerpc64le-linux-gcc -O2 -c -o /dev/null dcn_calcs.i || exit ${?}
 
-/home/nathan/cbl/toolchains/llvm-tot/bin/clang -O2 --target=powerpc64le-linux-gnu --prefix=/home/nathan/cbl/toolchains/binutils/bin/ --gcc-toolchain=/home/nathan/cbl/toolchains/binutils -no-integrated-as -mno-vsx -c -o /dev/null dcn_calcs.i
+/home/nathan/cbl/toolchains/llvm-tot/bin/clang -O2 --target=powerpc64le-linux-gnu --prefix=/home/nathan/cbl/toolchains/binutils/bin/ --gcc-toolchain=/home/nathan/cbl/toolchains/binutils -no-integrated-as -mno-vsx -c -o /dev/null dcn_calcs.i |& grep "error in backend: Cannot select:"
