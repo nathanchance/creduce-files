896d615eb2e58ee0ba62b499fbc0e333b15abdc5
Nathan Chancellor <natechancellor@gmail.com>
2020-03-23 23:15:40 -0700

    creduce-files: Add initial files for Fedora PowerPC64LE AMDGPU crash
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

---
+++ fedora-ppc64le-amdgpu/test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+/home/nathan/toolchains/gcc/9.3.0/bin/powerpc64le-linux-gcc -O2 -c -o /dev/null dcn_calcs.i || exit ${?}
+
+/home/nathan/cbl/toolchains/llvm-tot/bin/clang -O2 --target=powerpc64le-linux-gnu --prefix=/home/nathan/cbl/toolchains/binutils/bin/ --gcc-toolchain=/home/nathan/cbl/toolchains/binutils -no-integrated-as -mno-vsx -c -o /dev/null dcn_calcs.i
