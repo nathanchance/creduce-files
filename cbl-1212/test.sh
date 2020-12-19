dd4661ef613caf002e5cf9d9388fefbd116bd215
Nathan Chancellor <natechancellor@gmail.com>
2020-12-18 22:30:53 -0700

    creduce-files: cbl-1212: Results of reduction
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- cbl-1212/test.sh
+++ cbl-1212/test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -x
+
+rm gcc-mremap.o clang-mremap.o clang-ubsan-mremap.o
+
 gcc -O2 -c -o gcc-mremap.o mremap.i || exit ${?}
 
 /home/nathan/cbl/toolchains/llvm-20201216-2033-f03609b5c7531061be659e36824d37ef86a1fdf4/bin/clang -O2 -c -o clang-mremap.o mremap.i || exit ${?}
