9d77df633137562d0201e91215d2f11ab2c39e45
Nathan Chancellor <nathan@kernel.org>
2021-02-20 17:27:21 -0700

    creduce-files: Add initial files for https://github.com/llvm/llvm-project/commit/bdf6fbc939646b52af61c0bae7335623a8be59f4
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ bdf6fbc939646b52af61c0bae7335623a8be59f4/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -x
+
+"${HOME}"/toolchains/gcc/10.2.0/bin/powerpc-linux-gcc -fno-strict-aliasing -fno-common -fshort-wchar -fno-PIE -std=gnu89 -mbig-endian -m32 -msoft-float -pipe -ffixed-r2 -mmultiple -mno-readonly-in-sdata -mcpu=powerpc64 -mno-altivec -mno-vsx -fno-dwarf2-cfi-asm -mno-string -fno-delete-null-pointer-checks -O2 -fno-allow-store-data-races -fno-stack-protector -fomit-frame-pointer -fno-var-tracking-assignments -fno-strict-overflow -fno-merge-all-constants -fmerge-constants -fno-stack-check -fconserve-stack -c -o /dev/null gup.i &>/dev/null || exit ${?}
+
+"${HOME}"/cbl/toolchains/llvm-12.0.0-rc1/bin/clang -fno-strict-aliasing -fno-common -fshort-wchar -fno-PIE -std=gnu89 --target=powerpc-linux-gnu --prefix=${HOME}/cbl/toolchains/llvm-binutils/bin/powerpc-linux-gnu- --gcc-toolchain=${HOME}/cbl/toolchains/llvm-binutils -no-integrated-as -mbig-endian -m32 -msoft-float -mcpu=powerpc64 -mno-altivec -mno-vsx -mno-spe -fno-dwarf2-cfi-asm -fno-delete-null-pointer-checks -O2 -fno-stack-protector -fomit-frame-pointer -fno-strict-overflow -fno-merge-all-constants -fno-stack-check -c -o /dev/null gup.i &>/dev/null || exit ${?}
+
+"${HOME}"/cbl/github/tc-build/build/llvm/stage1/bin/clang -fno-strict-aliasing -fno-common -fshort-wchar -fno-PIE -std=gnu89 --target=powerpc-linux-gnu --prefix=${HOME}/cbl/toolchains/llvm-binutils/bin/powerpc-linux-gnu- --gcc-toolchain=${HOME}/cbl/toolchains/llvm-binutils -no-integrated-as -mbig-endian -m32 -msoft-float -mcpu=powerpc64 -mno-altivec -mno-vsx -mno-spe -fno-dwarf2-cfi-asm -fno-delete-null-pointer-checks -O2 -fno-stack-protector -fomit-frame-pointer -fno-strict-overflow -fno-merge-all-constants -fno-stack-check -c -o /dev/null gup.i |& grep "Shouldn't have kept evaluating on failure"
