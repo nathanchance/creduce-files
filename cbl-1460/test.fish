c96ccf803db2dfca10fcb0cfd655c5edb33bfe4f
Nathan Chancellor <nathan@kernel.org>
2021-09-20 22:58:30 -0700

    cbl-1460: Start with initial error
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- cbl-1460/test.fish
+++ cbl-1460/test.fish
@@ -2,13 +2,23 @@
 
 set fish_trace 1
 
-$GCC_TC_FOLDER/11.2.0/bin/x86_64-linux-gcc -O2 -c -o /dev/null simple-card-utils.i &>/dev/null; or exit
+# GCC should pass (initial compile smoke test)
+$GCC_TC_FOLDER/11.2.0/bin/x86_64-linux-gcc -O2 -c -o /dev/null kfd_mqd_manager.i &>/dev/null; or exit
 
-# No Polly
-$CBL_GIT/tc-build/build/llvm/stage1/bin/clang -std=gnu89 -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -fcf-protection=none -m64 -falign-loops=1 -mno-80387 -mstack-alignment=8 -mtune=generic -mno-red-zone -mcmodel=kernel -fno-asynchronous-unwind-tables -fno-delete-null-pointer-checks -Wno-frame-address -Wno-address-of-packed-member -O2 -Wframe-larger-than=1024 -fno-stack-protector -Wno-format-invalid-specifier -Wno-gnu -mno-global-merge -Wno-unused-but-set-variable -Wno-unused-const-variable -fomit-frame-pointer -fno-stack-clash-protection -flto -fvisibility=hidden -fno-strict-overflow -fno-stack-check -c -o /dev/null simple-card-utils.i &>/dev/null; or exit
+set llvm_bin $TMP_FOLDER/llvm-85b4b21c8bbad346d58a30154d2767c39cf3285a/bin
 
-# Just Polly
-$CBL_GIT/tc-build/build/llvm/stage1/bin/clang -std=gnu89 -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -fcf-protection=none -m64 -falign-loops=1 -mno-80387 -mstack-alignment=8 -mtune=generic -mno-red-zone -mcmodel=kernel -fno-asynchronous-unwind-tables -fno-delete-null-pointer-checks -Wno-frame-address -Wno-address-of-packed-member -O2 -Wframe-larger-than=1024 -fno-stack-protector -Wno-format-invalid-specifier -Wno-gnu -mno-global-merge -Wno-unused-but-set-variable -Wno-unused-const-variable -fomit-frame-pointer -fno-stack-clash-protection -flto -fvisibility=hidden -fno-strict-overflow -fno-stack-check -mllvm -polly -c -o /dev/null simple-card-utils.i &>/dev/null; or exit
+# No funky compiler flags should pass
+$llvm_bin/clang -fno-strict-aliasing -fno-common -fshort-wchar -fno-PIE -std=gnu89 -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -fcf-protection=none -m64 -falign-loops=1 -mno-80387 -mstack-alignment=8 -mtune=generic -mno-red-zone -mcmodel=kernel -fno-asynchronous-unwind-tables -fno-delete-null-pointer-checks -O2 -fno-stack-protector -fomit-frame-pointer -fno-stack-clash-protection -flto -fvisibility=hidden -fno-strict-overflow -fno-stack-check -c -o kfd_mqd_manager.o kfd_mqd_manager.i &>/dev/null; or exit
 
-# Polly + invariant load hoisting
-$CBL_GIT/tc-build/build/llvm/stage1/bin/clang -std=gnu89 -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -fcf-protection=none -m64 -falign-loops=1 -mno-80387 -mstack-alignment=8 -mtune=generic -mno-red-zone -mcmodel=kernel -fno-asynchronous-unwind-tables -fno-delete-null-pointer-checks -Wno-frame-address -Wno-address-of-packed-member -O2 -Wframe-larger-than=1024 -fno-stack-protector -Wno-format-invalid-specifier -Wno-gnu -mno-global-merge -Wno-unused-but-set-variable -Wno-unused-const-variable -fomit-frame-pointer -fno-stack-clash-protection -flto -fvisibility=hidden -fno-strict-overflow -fno-stack-check -mllvm -polly -mllvm -polly-invariant-load-hoisting -c -o /dev/null simple-card-utils.i &| grep '"Cannot split an edge from a CallBrInst"'
+$llvm_bin/llvm-ar cDPrsT amdgpu.o kfd_mqd_manager.o &>/dev/null; or exit
+
+$llvm_bin/ld.lld -m elf_x86_64 -mllvm -import-instr-limit=5   -r -o amdgpu.lto.o  --whole-archive amdgpu.o; or exit
+
+rm *.o
+
+# Adding all the flags should show the error message
+$llvm_bin/clang -fno-strict-aliasing -fno-common -fshort-wchar -fno-PIE -std=gnu89 -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -fcf-protection=none -m64 -falign-loops=1 -mno-80387 -mstack-alignment=8 -mtune=generic -mno-red-zone -mcmodel=kernel -fno-asynchronous-unwind-tables -fno-delete-null-pointer-checks -O2 -fno-stack-protector -fomit-frame-pointer -fno-stack-clash-protection -flto -fvisibility=hidden -fno-strict-overflow -fno-stack-check -march=native -mtune=native -mllvm -polly -mllvm -polly-parallel -mllvm -polly-vectorizer=stripmine -mllvm -polly-omp-backend=LLVM -mllvm -polly-num-threads=24 -mllvm -polly-scheduling=dynamic -mllvm -polly-scheduling-chunksize=1 -mllvm -polly-ast-use-context -mllvm -polly-invariant-load-hoisting -mllvm -polly-opt-fusion=max -mllvm -polly-run-inliner -mllvm -polly-run-dce -fno-math-errno -fno-trapping-math -falign-functions=32 -fno-semantic-interposition -c -o kfd_mqd_manager.o kfd_mqd_manager.i &>/dev/null; or exit
+
+$llvm_bin/llvm-ar cDPrsT amdgpu.o kfd_mqd_manager.o &>/dev/null; or exit
+
+$llvm_bin/ld.lld -m elf_x86_64 -mllvm -import-instr-limit=5   -r -o amdgpu.lto.o  --whole-archive amdgpu.o &| grep "Cannot take the address of an inline asm"
