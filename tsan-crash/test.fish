b7d5e37f40a362d408c9ac309a670d13516eb804
Nathan Chancellor <nathan@kernel.org>
2021-11-23 08:43:46 -0700

    tsan-crash: Results of reduction #2
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- tsan-crash/test.fish
+++ tsan-crash/test.fish
@@ -6,8 +6,6 @@ for file in core timekeeping
     end
 end
 
-set fish_trace 1
-
 # Smoke test without LTO
 clang -fshort-wchar -O2 -fno-stack-protector -Werror -Wuninitialized -Wno-error={unused-value,constant-logical-operand,address-of-packed-member} -c -o core.{o,i} &>/dev/null; or exit
 clang -fshort-wchar -O2 -fsanitize=thread -Werror -Wuninitialized -Wno-error={unused-value,constant-logical-operand,address-of-packed-member} -c -o timekeeping.{o,i} &>/dev/null; or exit
@@ -16,13 +14,9 @@ clang -fshort-wchar -O2 -fsanitize=thread -Werror -Wuninitialized -Wno-error={un
 clang -fshort-wchar -O2 -flto -fno-stack-protector -Werror -Wuninitialized -Wno-error={unused-value,constant-logical-operand,address-of-packed-member} -c -o core.{o,i} &>/dev/null; or exit
 clang -fshort-wchar -O2 -flto -fsanitize=thread -Werror -Wuninitialized -Wno-error={unused-value,constant-logical-operand,address-of-packed-member} -c -o timekeeping.{o,i} &>/dev/null; or exit
 
-llvm-ar cDPrST kcsan-built-in.a core.o; or exit
-llvm-ar cDPrST time-built-in.a timekeeping.o; or exit
+llvm-ar cDPrST built-in.a {core,timekeeping}.o; or exit
 
-ld.lld -m elf_x86_64 -r -o /dev/null --whole-archive kcsan-built-in.a; or exit
-ld.lld -m elf_x86_64 -r -o /dev/null --whole-archive time-built-in.a; or exit
-
-ld.lld -m elf_x86_64 -r -o /dev/null --whole-archive {kcsan,time}-built-in.a &| grep "inlinable function call in a function with debug info must have a"
+ld.lld -m elf_x86_64 -r -o /dev/null --whole-archive built-in.a &| grep "inlinable function call in a function with debug info must have a"
 set my_pipe $pipestatus
 if test $my_pipe[1] -ne 0
     exit 1
@@ -34,10 +28,6 @@ end
 clang -fshort-wchar -O2 -flto -g -fno-stack-protector -Werror -Wuninitialized -Wno-error={unused-value,constant-logical-operand,address-of-packed-member} -c -o core.{o,i} &>/dev/null; or exit
 clang -fshort-wchar -O2 -flto -g -fsanitize=thread -Werror -Wuninitialized -Wno-error={unused-value,constant-logical-operand,address-of-packed-member} -c -o timekeeping.{o,i} &>/dev/null; or exit
 
-llvm-ar cDPrST kcsan-built-in.a core.o; or exit
-llvm-ar cDPrST time-built-in.a timekeeping.o; or exit
+llvm-ar cDPrST built-in.a {core,timekeeping}.o; or exit
 
-ld.lld -m elf_x86_64 -r -o /dev/null --whole-archive kcsan-built-in.a; or exit
-ld.lld -m elf_x86_64 -r -o /dev/null --whole-archive time-built-in.a; or exit
-
-ld.lld -m elf_x86_64 -r -o /dev/null --whole-archive {kcsan,time}-built-in.a &| grep "inlinable function call in a function with debug info must have a"
+ld.lld -m elf_x86_64 -r -o /dev/null --whole-archive built-in.a &| grep "inlinable function call in a function with debug info must have a"
