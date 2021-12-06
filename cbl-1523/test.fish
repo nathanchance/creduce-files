3d2cc20583385b7eaf1ca2d1632644003124d0e9
Nathan Chancellor <nathan@kernel.org>
2021-12-06 13:44:42 -0700

    cbl-1523: Results of reduction
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- cbl-1523/test.fish
+++ cbl-1523/test.fish
@@ -4,7 +4,7 @@ set clang $CBL_TC_BIN/clang --target=aarch64-linux-gnu
 set io_flags -c -o /dev/null test.i
 
 # There should be no new warnings
-aarch64-suse-linux-gcc -Os -Werror{,=unused-value} -Wno-error=attributes $io_flags; or exit
+aarch64-linux-gnu-gcc -Os -Werror{,=unused-value} -Wno-error=attributes $io_flags; or exit
 $clang -Os -Werror -Wno-error={gnu,pointer-sign,unused-value} $io_flags; or exit
 
 # The problematic options should not cause any crashes by themselves, otherwise it is not interesting
