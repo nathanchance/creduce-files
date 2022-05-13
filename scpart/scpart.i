5756ce351b9408e588fe1c5ee28f8f8e19182526
Nathan Chancellor <nathan@kernel.org>
2022-05-13 13:02:37 -0700

    scparts: Do reduction with i386 file
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- scpart/scpart.i
+++ scpart/scpart.i
@@ -3,8 +3,9 @@ struct {
   int part_offs;
   int part_bytes;
 } scpart_scan_partmap_tmpdesc;
+int scpart_scan_partmap_master_0;
 long long scpart_scan_partmap_offs;
 void scpart_scan_partmap() {
-  while (scpart_scan_partmap_offs)
+  while (scpart_scan_partmap_offs < scpart_scan_partmap_master_0)
     scpart_scan_partmap_offs += sizeof scpart_scan_partmap_tmpdesc;
 }
--- scpart/test.fish
+++ scpart/test.fish
@@ -1,19 +1,18 @@
 #!/usr/bin/env fish
 
-set clang_flags --target=arm-linux-gnueabi
-set common_flags -O2 -c -o scpart.{o,i}
+set common_flags -fno-PIC -m32 -O2 -c -o scpart.{o,i}
 
-arm-none-eabi-gcc $common_flags; or return
-llvm-nm -S scpart.o &| grep __aeabi_uldivmod
+gcc $common_flags; or return
+llvm-nm -S scpart.o &| grep __udivdi3
 if test "$pipestatus" = "0 0"
     exit 1
 end
 llvm-objdump -dr scpart.o
 
 rm scpart.o
-clang $clang_flags $common_flags; or return
+clang $common_flags; or return
 llvm-objdump -dr scpart.o
-llvm-nm -S scpart.o &| grep __aeabi_uldivmod
+llvm-nm -S scpart.o &| grep __udivdi3
 if test "$pipestatus" = "0 0"
     exit 0
 else
